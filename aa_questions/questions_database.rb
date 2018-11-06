684a53817a1c06f30ad2ca81a2f9a99f360ee0b2
Steven Inouye <steveninouye@msn.com>
2018-11-06 11:44:54 -0800

    Add questons database rb file

---
+++ aa_questions/questions_database.rb
@@ -0,0 +1,30 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Play
+  attr_accessor :title, :year, :playwright_id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def initialize(options)
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
